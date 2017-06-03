ad867e908e78266b40361ab4b1264557ee67e134
ashwinshenoy <ashwinkshenoy@gmail.com>
2017-06-03 20:11:17 +0530

    footer completed and navbar animation added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,9 +12,28 @@ $(document).ready(function(){
     $('.content[data-id=' + (currentSlide + 1) + ']').addClass('slide-box');
     console.log(currentSlide);
   });
-  $('.content').bind('click', function(){
+  $('.content').bind('click', function() {
     console.log($(this).data('id'));
     var index = $(this).data('id')-1;
     $('.slider1').slick('slickGoTo', index, true);
   });
+
+  // menu items
+  $('.navbar-toggle').bind('click', function() {
+    var el = $(this);
+    if (el.hasClass('active')){
+      el.addClass('collapsed');
+      el.removeClass('active');
+      $('.navbar-collapse').removeClass('in');
+      $('body').removeClass('body-scroll');
+      $('.overlay').hide();
+    } else {
+      $('body').addClass('body-scroll');
+      el.addClass('active');
+      $('.navbar-collapse').addClass('in');
+      el.removeClass('collapsed');
+      $('.overlay').show();
+    }
+  });
+
 });
